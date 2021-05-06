92a3f8d6e958c421dafc3a7dae637485356146cc
seireitou <ausis@mail.com>
2021-05-06 17:06:57 -0500

    sets up index for posts controller and adds post.js file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(title: "Second Post", description: "more descriptions", img_url: "some url")
+Post.create(title: "Third Post", description: "more descriptions", img_url: "some url")
+Post.create(title: "Fourth Post", description: "more descriptions", img_url: "some url")
+Post.create(title: "Fifth Post", description: "more descriptions", img_url: "some url")
+Post.create(title: "Sixth Post", description: "more descriptions", img_url: "some url")
+Post.create(title: "Seventh Post", description: "more descriptions", img_url: "some url")
