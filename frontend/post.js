6843b9d8b080c5195318cdafab516f173ed8f9b7
seireitou <ausis@mail.com>
2021-05-06 19:49:37 -0500

    creations function appendPosts and creates comment.js file

--- frontend/post.js
+++ frontend/post.js
@@ -1,9 +1,15 @@
 function fetchPosts() {
     fetch("http://127.0.0.1:3000/posts")
     .then(resp => resp.json())
-    .then(posts => {
-        console.log(posts)
-    })
+    .then(appendPosts)
 }
 
-fetchPosts()
+function appendPosts(posts) {
+    const postsDiv = document.querySelector('#posts')
+    for (let post of posts) {
+        const li = document.createElement("li")
+        li.innerText = post.title
+        postsDiv.append(li)
+        appendComments(post.comments)
+    }
+}
